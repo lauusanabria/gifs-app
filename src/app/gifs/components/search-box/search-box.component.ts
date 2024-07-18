e050b700ce7db432cf74f4ef0b525668d1c00763
Laura Sanabria <laura_sanabria@epam.com>
2024-07-17 23:50:00 -0500

    created gif search history

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
-  styleUrl: './search-box.component.css'
+  styleUrl: './search-box.component.css',
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
 }
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+  private _tagsHistory: string[] = [];
+
+  constructor() {}
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  searchTag(tag: string) {
+    this._tagsHistory.unshift(tag);
+  }
+}
