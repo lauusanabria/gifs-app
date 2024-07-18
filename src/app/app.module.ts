56a53982ef2f18c9cc4771ae71d84f863b804aa4
Laura Sanabria <laura_sanabria@epam.com>
2024-07-18 09:09:53 -0500

    added search service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    GifsModule,
-    SharedModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, GifsModule, SharedModule],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,37 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const GIPHY_API_KEY: string = '5T3DUZsV2sh4vkkNsTJRFTUgh7msDeHe';
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private serviceBase: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string) {
+  searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeTag(tag);
+
+    const params = new HttpParams()
+      .set('api_key', GIPHY_API_KEY)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http
+      .get(`${this.serviceBase}/search`, { params })
+      .subscribe((response) => console.log(response));
+
+    /*
+    fetch(
+      'https://api.giphy.com/v1/gifs/search?api_key=5T3DUZsV2sh4vkkNsTJRFTUgh7msDeHe&q=valorant&limit=10'
+    )
+      .then((resp) => resp.json())
+      .then((data) => console.log(data));*/
   }
 
   private organizeTag(tag: string) {
